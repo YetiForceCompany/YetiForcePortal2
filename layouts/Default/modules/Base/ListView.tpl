6a67ee51401f95be86fa9534e1614a92b04394f5
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-08-17 23:06:52 +0200

    Added part display a list of records in the API

--- layouts/Default/modules/Base/ListView.tpl
+++ layouts/Default/modules/Base/ListView.tpl
@@ -1,4 +1,23 @@
 {strip}
-	xx
+	<div class="row">
+		<table class="table">
+			<thead>
+				<tr>
+					<th>#</th>
+					<th>First Name</th>
+					<th>Last Name</th>
+					<th>Username</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<th scope="row">1</th>
+					<td>Mark</td>
+					<td>Otto</td>
+					<td>@mdo</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>
 {/strip}
 
--- modules/Base/views/ListView.php
+++ modules/Base/views/ListView.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Users view class
+ * List view class
  * @package YetiForce.View
  * @license licenses/License.html
  * @author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
@@ -15,7 +15,13 @@ class ListView extends Index
 	public function process(Core\Request $request)
 	{
 		$module = $request->getModule();
+		$api = Core\Api::getInstance();
+		$respons = $api->call($module . '/GetRecordsList', [], 'get');
+
+
 		$viewer = $this->getViewer($request);
+		$viewer->assign('HEADER', Core\Language::getLanguage());
+		$viewer->assign('RECORDS', Core\Language::getLanguage());
 		$viewer->view('ListView.tpl', $module);
 	}
 }
