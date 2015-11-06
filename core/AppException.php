eddcba27d581a2f71b8fb441248469fa59f426c1
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2015-11-06 23:30:00 +0100

    Improved error message

--- core/AppException.php
+++ core/AppException.php
@@ -13,7 +13,7 @@ class AppException extends \Exception
 
 	public function __construct($message, $code = 200, Exception $previous = null)
 	{
-		parent::__construct($message, $code, $previous);
+		die($message);
 	}
 }
 
