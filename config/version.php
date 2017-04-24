16cc387b4d05008df14d173356d0b0e78df93b8c
Michał Lorencik <michal11991@gmail.com>
2017-04-24 16:14:23 +0200

    Added part of fields ReferenceProcess, ReferenceSubProcess, ReferenceLink and fix Reference

--- config/version.php
+++ config/version.php
@@ -7,7 +7,7 @@
  */
 defined('_EXEC') or die;
 
-$version = '0.0.8';
+$version = '0.0.9';
 $patchVersion = '20170424';
 
 define('VERSION', $version);
--- modules/Base/models/Field.php
+++ modules/Base/models/Field.php
@@ -123,7 +123,7 @@ class Field extends \Core\BaseModel
 	public function getTemplate()
 	{
 		$type = ucfirst($this->get('type'));
-		if (!in_array($type, ['Reference', 'Picklist', 'Owner', 'Boolean', 'Text', 'Url', 'SharedOwner', 'Multipicklist', 'Email', 'Percentage', 'Phone', 'Date', 'Time', 'Image'])) {
+		if (!in_array($type, ['Reference', 'ReferenceProcess', 'ReferenceSubProcess', 'ReferenceLink', 'Picklist', 'Owner', 'Boolean', 'Text', 'Url', 'SharedOwner', 'Multipicklist', 'Email', 'Percentage', 'Phone', 'Date', 'Time'])) {
 			$type = 'String';
 		}
 		return "fieldtypes/$type.tpl";
