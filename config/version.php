7a98de33b0bcb2aa3cf9c703771aa40660647ebb
Michał Lorencik <michal11991@gmail.com>
2017-05-15 12:20:06 +0200

    Added BaseValidator and FieldValidator

--- config/version.php
+++ config/version.php
@@ -6,7 +6,7 @@
  * @package YetiForce.Configuration
  * @author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
  */
-$version = '0.0.63';
+$version = '0.0.64';
 $patchVersion = '20170515';
 
 define('VERSION', $version);
