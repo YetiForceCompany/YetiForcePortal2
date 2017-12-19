c9c48394570c3f76f665475da7f5b4269eb222c9
areksolek <a.solek@yetiforce.com>
2017-12-19 10:18:27 +0100

    Changed license -5-

--- modules/Base/Model/Record.php
+++ modules/Base/Model/Record.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Basic Record Model Class
+ * Basic record model class
  * @package YetiForce.Model
  * @copyright YetiForce Sp. z o.o.
- * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
+ * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
  * @author Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
  */
 namespace YF\Modules\Base\Model;
--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
  * Main file
  * @package YetiForce.Portal
  * @copyright YetiForce Sp. z o.o.
- * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
+ * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
  * @author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
  */
 error_reporting(E_ALL);
