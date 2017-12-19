f43898aadb06f4c118164a26e04c5b46ea9164f9
areksolek <a.solek@yetiforce.com>
2017-12-19 09:13:31 +0100

    Changed license

--- core/Loader.php
+++ core/Loader.php
@@ -2,7 +2,7 @@
 /**
  * System files loader
  * @copyright YetiForce Sp. z o.o.
- * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
+ * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
  * @package YetiForce.Core
  * @author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
  */
