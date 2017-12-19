9dfe44f862f7283e12daae63e5d0b8077c0006d4
areksolek <a.solek@yetiforce.com>
2017-12-19 10:01:57 +0100

    Changed license -4-

--- modules/Base/Action/Base.php
+++ modules/Base/Action/Base.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Abstract Action Controller Class
+ * Abstract action controller class
  * @package YetiForce.Actions
  * @copyright YetiForce Sp. z o.o.
- * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
+ * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
  * @author Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
  */
 namespace YF\Modules\Base\Action;
--- modules/Base/FieldTypes/BaseField.php
+++ modules/Base/FieldTypes/BaseField.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * Basic Field Model Class
+ * Basic field model class
  * @package YetiForce.FieldTypes
  * @copyright YetiForce Sp. z o.o.
- * @license YetiForce Public License 2.0 (licenses/License.html or yetiforce.com)
+ * @license YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
  * @author Michał Lorencik <m.lorencik@yetiforce.com>
  */
 namespace YF\Modules\Base\FieldTypes;
