8dc536eeb3bc9db0e977b76050649e49be375007
Arkadiusz Adach <a.adach@yetiforce.com>
2019-06-26 15:46:28 +0200

    Grouped products

--- public_html/layouts/Default/modules/Products/resources/Preview.js
+++ public_html/layouts/Default/modules/Products/resources/Preview.js
@@ -1,11 +1,9 @@
 /* {[The file is published on the basis of YetiForce Public License 3.0 that can be found in the following directory: licenses/LicenseEN.txt or yetiforce.com]} */
-"use strict";
+'use strict';
 window.Products_Preview_Js = class {
-	constructor(container = $(".js-preview")) {
+	constructor(container = $('.js-preview')) {
 		this.container = container;
-		this.shoppingCartBadge = $(
-			".js-body-header .js-shopping-cart .js-badge"
-		);
+		this.shoppingCartBadge = $('.js-body-header .js-shopping-cart .js-badge');
 	}
 	cartMethod(mode, recordId, params = {}) {
 		const deferred = $.Deferred();
@@ -13,7 +11,7 @@ window.Products_Preview_Js = class {
 			$.extend(
 				{
 					module: app.getModuleName(),
-					action: "ShoppingCart",
+					action: 'ShoppingCart',
 					mode: mode,
 					record: recordId
 				},
@@ -24,15 +22,18 @@ window.Products_Preview_Js = class {
 		});
 		return deferred.promise();
 	}
+	getCartItem(element) {
+		return $(element).closest('.js-cart-item');
+	}
 	registerAmountChange() {
-		this.container.find(".js-amount-inc").on("click", e => {
-			let amount = this.container.find(".js-amount");
+		this.container.find('.js-amount-inc').on('click', e => {
+			let amount = this.getCartItem(e.currentTarget).find('.js-amount');
 			let amountVal = amount.val();
 			amountVal++;
 			amount.val(amountVal);
 		});
-		this.container.find(".js-amount-dec").on("click", e => {
-			let amount = this.container.find(".js-amount");
+		this.container.find('.js-amount-dec').on('click', e => {
+			let amount = this.getCartItem(e.currentTarget).find('.js-amount');
 			let amountVal = amount.val();
 			amountVal--;
 			if (amountVal >= 0) {
@@ -41,57 +42,93 @@ window.Products_Preview_Js = class {
 		});
 	}
 	registerButtonAddToCart() {
-		this.container.find(".js-add-to-cart").on("click", e => {
+		this.container.find('.js-add-to-cart').on('click', e => {
 			let product = this.container;
-			let amount = product.find(".js-amount").val();
-			if (parseInt(amount) <= 0) {
-				return
+			let amountVal = product.find('.js-amount').val();
+			if (parseInt(amountVal) <= 0) {
+				return;
 			}
-			let amountInShoppingCart = parseFloat(
-				product.data("amountInShoppingCart")
-			);
-			let qtyinstock = parseFloat(product.data("qtyinstock"));
-			if (
-				this.checkStockLevels &&
-				qtyinstock - amountInShoppingCart - amount < 0
-			) {
+			let amountInShoppingCart = parseFloat(product.data('amountInShoppingCart'));
+			let qtyinstock = parseFloat(product.data('qtyinstock'));
+			if (this.checkStockLevels && qtyinstock - amountInShoppingCart - amountVal < 0) {
 				Vtiger_Helper_Js.showPnotify({
-					text: app.translate("JS_NO_SUCH_QUANTITY"),
-					type: "error"
+					text: app.translate('JS_NO_SUCH_QUANTITY'),
+					type: 'error'
 				});
 			} else {
-				const amount = product.find(".js-amount").val()
-				this.cartMethod("addToCart", this.container.find('.js-preview-record').val(), {
-					amount: product.find(".js-amount").val(),
-					priceNetto: product.data("priceNetto"),
-					priceGross: product.data("priceGross"),
+				const amountVal = product.find('.js-amount').val();
+				this.cartMethod('addToCart', this.container.find('.js-preview-record').val(), {
+					amount: product.find('.js-amount').val(),
+					priceNetto: product.data('priceNetto'),
+					priceGross: product.data('priceGross')
 				}).done(data => {
-					if (data["result"]['error']) {
+					if (data['result']['error']) {
 						Vtiger_Helper_Js.showPnotify({
-							text: data["result"]['error'],
-							type: "error"
+							text: data['result']['error'],
+							type: 'error'
 						});
 					} else {
-						this.shoppingCartBadge.text(
-							data["result"]["numberOfItems"]
-						);
-						const notifyText = amount > 1 ? app.translate('JS_ADDED_ITEMS_TO_CART').replace('${amount}', amount) : app.translate('JS_ADDED_ITEM_TO_CART')
+						this.shoppingCartBadge.text(data['result']['numberOfItems']);
+						const notifyText =
+							amountVal > 1
+								? app.translate('JS_ADDED_ITEMS_TO_CART').replace('${amount}', amountVal)
+								: app.translate('JS_ADDED_ITEM_TO_CART');
 						Vtiger_Helper_Js.showPnotify({
 							text: notifyText,
-							type: "success"
+							type: 'success'
 						});
-						product.data(
-							"amountInShoppingCart",
-							amountInShoppingCart + amount
-						);
+						product.data('amountInShoppingCart', amountInShoppingCart + amountVal);
+					}
+				});
+			}
+		});
+	}
+	registerButtonAddToCartForBundles() {
+		this.container.find('.js-add-to-cart-bundles').on('click', e => {
+			let product = this.getCartItem(e.currentTarget);
+			let amount = product.find('.js-amount').val();
+			if (parseInt(amount) <= 0) {
+				return;
+			}
+			let amountInShoppingCart = parseFloat(product.data('amountInShoppingCart'));
+			let qtyinstock = parseFloat(product.data('qtyinstock'));
+			if (this.checkStockLevels && qtyinstock - amountInShoppingCart - amount < 0) {
+				Vtiger_Helper_Js.showPnotify({
+					text: app.translate('JS_NO_SUCH_QUANTITY'),
+					type: 'error'
+				});
+			} else {
+				const amount = product.find('.js-amount').val();
+				this.cartMethod('addToCart', product.data('id'), {
+					amount: amount,
+					priceNetto: product.data('priceNetto'),
+					priceGross: product.data('priceGross')
+				}).done(data => {
+					if (data['result']['error']) {
+						Vtiger_Helper_Js.showPnotify({
+							text: data['result']['error'],
+							type: 'error'
+						});
+					} else {
+						this.shoppingCartBadge.text(data['result']['numberOfItems']);
+						const notifyText =
+							amount > 1
+								? app.translate('JS_ADDED_ITEMS_TO_CART').replace('${amount}', amount)
+								: app.translate('JS_ADDED_ITEM_TO_CART');
+						Vtiger_Helper_Js.showPnotify({
+							text: notifyText,
+							type: 'success'
+						});
+						product.data('amountInShoppingCart', amountInShoppingCart + amount);
 					}
 				});
 			}
 		});
 	}
 	registerEvents() {
-		this.container = $(".js-preview");
+		this.container = $('.js-preview');
 		this.registerAmountChange();
 		this.registerButtonAddToCart();
+		this.registerButtonAddToCartForBundles();
 	}
 };
