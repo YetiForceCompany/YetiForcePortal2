f2e16e227c9528ce17d8cc789bb21afeb39460a7
Mariusz Krzaczkowski <m.krzaczkowski@yetiforce.com>
2021-05-12 13:22:27 +0200

    Compiled js/css files

--- modules/Products/View/Preview.php
+++ modules/Products/View/Preview.php
@@ -80,6 +80,10 @@ class Preview extends \App\Controller\View
 		$products = [];
 		foreach ($productBundles as $key => $row) {
 			$recordModel = Record::getInstance($moduleName);
+			if (isset($value['recordLabel'])) {
+				$recordModel->setName($value['recordLabel']);
+				unset($value['recordLabel']);
+			}
 			$recordModel->setData($row['data']);
 			$recordModel->setRawData($row['rawData']);
 			$products[$key] = $recordModel;
