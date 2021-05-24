3534e9b14f7eab68a5c04d7f4991838e36bc0d56
Arek Solek <arkadiusz_s9887@wp.pl>
2021-05-24 11:33:13 +0200

    Unified SharedOwner, Owner field (#293)
    * Unified SharedOwner, Owner field
    
    * Corrected commit

---
+++ modules/Base/FieldTypes/SharedOwnerField.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Shared owner field file.
+ *
+ * @copyright YetiForce Sp. z o.o.
+ * @license   YetiForce Public License 3.0 (licenses/LicenseEN.txt or yetiforce.com)
+ * @author    Arkadiusz Sołek <a.solek@yetiforce.com>
+ */
+
+namespace YF\Modules\Base\FieldTypes;
+
+/**
+ * Shared owner field class.
+ */
+class SharedOwner extends BaseField
+{
+	/** {@inheritdoc} */
+	public function getTemplateName(): string
+	{
+		return 'Field/Edit/Owner.tpl';
+	}
+}
