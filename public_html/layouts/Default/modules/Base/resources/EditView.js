cd419767671dddc66aa1bdf81e7d735ca0b3bd6b
Arek Solek <arkadiusz_s9887@wp.pl>
2021-06-10 09:12:49 +0200

    Unified Tree field.

--- public_html/layouts/Default/modules/Base/resources/EditView.js
+++ public_html/layouts/Default/modules/Base/resources/EditView.js
@@ -111,7 +111,7 @@ jQuery.Class(
 						});
 						container.find('.js-tree-text').val(selectedCategories[0]['text']);
 						container.find('.js-tree-value').val(selectedCategories[0]['original']['tree']);
-						app.hideModalWindow(modalContainer);
+						app.hideModalWindow();
 					});
 				});
 			});
