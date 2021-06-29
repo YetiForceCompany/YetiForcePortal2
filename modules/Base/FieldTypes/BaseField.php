b4abad3c4a485cd776d68b2fa962d3395aaea994
Radosław Skrzypczak <r.skrzypczak@yetiforce.com>
2021-06-29 10:10:00 +0200

    Improved not supported fields for edit view

--- modules/Base/FieldTypes/BaseField.php
+++ modules/Base/FieldTypes/BaseField.php
@@ -30,7 +30,7 @@ class BaseField extends \App\BaseModel
 	protected $defaultOperator = 'a';
 
 	/** @var string[] Not supported field types. */
-	protected $notSupportedToEdit = ['multiCurrency', 'serverAccess', 'multiReference', 'barcode', 'changesJson', 'iban', 'token', 'currencyInventory', 'twitter', 'multiReferenceValue', 'password', 'sharedOwner', 'taxes', 'recurrence', 'meetingUrl', 'reminder'];
+	protected $notSupportedToEdit = ['multiCurrency', 'serverAccess', 'multiReference', 'barcode', 'changesJson', 'iban', 'token', 'currencyInventory', 'twitter', 'multiReferenceValue', 'password', 'sharedOwner', 'taxes', 'recurrence', 'meetingUrl', 'reminder', 'totalTime', 'fileLocationType', 'documentsFileUpload', 'multiowner', 'image', 'multiImage', 'userReference', 'currencyList', 'modules', 'inventoryLimit', 'multiEmail', 'multiDependField', 'smtp', 'multiDomain', 'magentoServer'];
 
 	/**
 	 * Function to set the name of the module to which the record belongs.
